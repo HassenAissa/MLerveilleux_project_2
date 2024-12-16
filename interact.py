90b6545014a87d275f735a3403226f769380ae8f
Pierrefast <pierre.fasterling@epfl.ch>
2024-12-16 18:45:50 +0000

    patch interact

--- interact.py
+++ interact.py
@@ -8,6 +8,8 @@ MAX_DATE = 2024
 MIN_DATE = 2013
 STOP_QUERY = "STOP"
 INPUT_SYMBOL = "~>"
+PROMPT_FRAME = "You are an AI language model based on the GPT-2 architecture. Your purpose is to generate human-like text based on the input you receive. Your task is to understand the context of the conversation or query and respond coherently and appropriately. You have knowledge on a wide range of topics, but your responses should be based on the data you were trained on, which has a cutoff date of [insert date]. When responding, ensure your answers are clear, relevant, and informative, adhering to the tone of the input provided."
+
 
 def choices(clist, text="Please choose one of the following options: "):
     print(text)
@@ -20,8 +22,9 @@ def choices(clist, text="Please choose one of the following options: "):
 def run_query(query,date, model, tokenizer, config, max_answer_len = 100, device="cuda"):
     ctx_window_size = config.sequence_length
     query_tokens = tokenizer.encode_ordinary(query)
+    prompt_frame_pad = tokenizer.encode_ordinary(PROMPT_FRAME)
     query_len = len(query_tokens)
-    query_tokens = tokenizer.encode(" ")*(ctx_window_size - query_len) + query_tokens
+    query_tokens = tokenizer.encode(" ")*(ctx_window_size - query_len - len(prompt_frame_pad)) + prompt_frame_pad + query_tokens
     query_tokens = torch.tensor(query_tokens, dtype=torch.int64)
     query_tokens = query_tokens.reshape((1,-1))
 

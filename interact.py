14776f3df06821a66566b4ffd332085ffb73a5c6
Astraval <fasterlingpierre@gmail.com>
2024-12-16 19:52:33 +0100

    Interact with model: final debug

--- interact.py
+++ interact.py
@@ -8,8 +8,8 @@ MAX_DATE = 2024
 MIN_DATE = 2013
 STOP_QUERY = "STOP"
 INPUT_SYMBOL = "~>"
-PROMPT_FRAME = "You are an AI language model based on the GPT-2 architecture. Your purpose is to generate human-like text based on the input you receive. Your task is to understand the context of the conversation or query and respond coherently and appropriately. You have knowledge on a wide range of topics, but your responses should be based on the data you were trained on, which has a cutoff date of [insert date]. When responding, ensure your answers are clear, relevant, and informative, adhering to the tone of the input provided."
-
+#PROMPT_FRAME = "You are an AI language model based on the GPT-2 architecture. Your purpose is to generate human-like text based on the input you receive. Your task is to understand the context of the conversation or query and respond coherently and appropriately. You have knowledge on a wide range of topics, but your responses should be based on the data you were trained on, which has a cutoff date of [insert date]. When responding, ensure your answers are clear, relevant, and informative, adhering to the tone of the input provided."
+PROMPT_FRAME_PATH="prompt_frame.txt"
 
 def choices(clist, text="Please choose one of the following options: "):
     print(text)
@@ -18,13 +18,18 @@ def choices(clist, text="Please choose one of the following options: "):
     while c== None or c.isnumeric() == False or int(c) >= len(clist):
         c = input(">")
     return clist[int(c)]
+def get_prompt_frame(path):
+    with open(path, "r") as file:
+        text = file.read()
+    return text
 
-def run_query(query,date, model, tokenizer, config, max_answer_len = 100, device="cuda"):
+def run_query(query,date, model, tokenizer, config, prompt_frame,max_answer_len = 100, device="cuda"):
     ctx_window_size = config.sequence_length
     query_tokens = tokenizer.encode_ordinary(query)
-    prompt_frame_pad = tokenizer.encode_ordinary(PROMPT_FRAME)
+    prompt_frame_pad = tokenizer.encode_ordinary(prompt_frame)
     query_len = len(query_tokens)
     query_tokens = tokenizer.encode(" ")*(ctx_window_size - query_len - len(prompt_frame_pad)) + prompt_frame_pad + query_tokens
+    query_tokens = query_tokens[-ctx_window_size:]
     query_tokens = torch.tensor(query_tokens, dtype=torch.int64)
     query_tokens = query_tokens.reshape((1,-1))
 
@@ -79,6 +84,7 @@ if __name__ == "__main__":
 
 
     max_len = config.sequence_length
+    prompt_frame = get_prompt_frame(PROMPT_FRAME_PATH)
     print("Loading tokenizer...")
     tokenizer = tiktoken.get_encoding("gpt2")
     print("done")
@@ -88,7 +94,7 @@ if __name__ == "__main__":
 
     query = input(INPUT_SYMBOL)
     while query != STOP_QUERY:
-        run_query(query, None, model, tokenizer, config)
+        run_query(query, None, model, tokenizer, config, prompt_frame)
         query = input(INPUT_SYMBOL)
 
 
